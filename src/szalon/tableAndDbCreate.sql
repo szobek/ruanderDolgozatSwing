d5ddd5c071856363c458b78e9e9294313953919e
Kunszt Norbert <kunszt.norbert@gmail.com>
2023-06-08 16:03:57 +0200

    change sql file with insert

--- src/szalon/tableAndDbCreate.sql
+++ src/szalon/tableAndDbCreate.sql
@@ -1,11 +1,16 @@
-create database oraSzalon;
+create database orabolt;
 
-use oraSzalon;
+use orabolt;
 
 create table orak (
 ID int auto_increment unique ,
-name vachar(50) not null,
-tipus varchar(10) not null,
+name varchar(50) not null,
+tipus varchar(20) not null,
 ar int,
 vizallo BOOLEAN default false
 );
+
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Festina', 'karóra', '110000', '1');
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Casio', 'karóra', '34000', '1');
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Rolex', 'karóra', '1000000', '1');
+INSERT INTO `orak` (`ID`, `name`, `tipus`, `ar`, `vizallo`) VALUES (NULL, 'Hama', 'ébresztõóra', '4400', '0');
